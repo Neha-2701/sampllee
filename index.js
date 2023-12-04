fe683e94b62c14cffd2cf7c547c4582000450ef6
Neha Garg <nehagarg@Nehas-MacBook-Air.local>
2023-12-04 12:59:25 +0530

    sample

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express();
+require('dotenv').config();
+const port = process.env.PORT || 3030 ;
+
+console.log(port)
+app.get('/', (req,resp)=>{
+    resp.send("done")
+})
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+  });
